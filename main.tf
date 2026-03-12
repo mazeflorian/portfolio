aff22eb331c8429998b35810b6baee2afe1ed23a
mazeflorien <mazeflorian475@gmail.com>
2026-03-12 16:44:03 +0100

    feat/infra/v1

--- main.tf
+++ main.tf
@@ -6,82 +6,90 @@ provider "aws" {
 
 data "aws_ami" "ubuntu" {
 
-    most_recent = true
+  most_recent = true
 
-    filter {
-      name = "name"
-      values = [var.ubuntu_ami_name]
-    }
+  filter {
+    name   = "name"
+    values = [var.ubuntu_ami_name]
+  }
 
-    owners = [ var.ubuntu_ami_owner ]
+  owners = [var.ubuntu_ami_owner]
 }
 
-resource "aws_vpc_security_group" "florian_security_group" {
-    name = "florian_security_group"
-    vpc_id = var.vpc_id
-  
+
+
+
+
+resource "aws_security_group" "florian_security_group" {
+  name   = "florian_security_group"
+  vpc_id = var.vpc_id
+
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_http" {
-  from_port = "80"
-  to_port = "80"
-  ip_protocol = "http"
-  cidr_ipv4 = "0.0.0.0/0"
+  from_port         = "80"
+  to_port           = "80"
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
   security_group_id = aws_security_group.florian_security_group.id
 }
 
 
 resource "aws_vpc_security_group_ingress_rule" "allow_icmp" {
-  ip_protocol = "icmp"
-  cidr_ipv4 = "0.0.0.0/0"
+  from_port         = -1
+  to_port           = -1
+  ip_protocol       = "icmp"
+  cidr_ipv4         = "0.0.0.0/0"
   security_group_id = aws_security_group.florian_security_group.id
 }
 
 
 
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
-  from_port = "22"
-  to_port = "22"
-  ip_protocol = "tcp"
-  cidr_ipv4 = "0.0.0.0/0"
+  from_port         = "22"
+  to_port           = "22"
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
   security_group_id = aws_security_group.florian_security_group.id
 }
 
 
 resource "aws_vpc_security_group_egress_rule" "allow_all" {
-    ip_protocol = "-1"
-    cidr_ipv4 = "0.0.0.0/0"
-    security_group_id = aws_security_group.florian_security_group.id
+  ip_protocol       = "-1"
+  cidr_ipv4         = "0.0.0.0/0"
+  security_group_id = aws_security_group.florian_security_group.id
 }
 
 
 resource "aws_subnet" "florian_subnet" {
-    vpc_id = var.vpc_id
-    cidr_block = "172.31.14.0/24"
+  vpc_id     = var.vpc_id
+  cidr_block = "172.31.14.0/24"
 
-  
+
 }
 
 
 
 resource "aws_key_pair" "florian_Keypair" {
-  key_name = "florian_key"
-  public_key = var.my_key
+  key_name   = "florian_key"
+  public_key = file("~/.ssh/id_ed25519.pub")
 }
 
 
 
 
 resource "aws_instance" "florian_instance" {
-    security_groups = [ aws_security_group.florian_security_group.id ]
-    subnet_id = aws_subnet.florian_subnet.id
-    associate_public_ip_address = true
-    ami = data.aws_ami.ubuntu.id
+  security_groups             = [aws_security_group.florian_security_group.id]
+  subnet_id                   = aws_subnet.florian_subnet.id
+  associate_public_ip_address = true
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = var.my_instance_type
+  key_name                    = aws_key_pair.florian_Keypair.key_name
 
 
 
-    tags = {
-        Name = "florian_instance"
-    }
-  
+  tags = {
+    Name = "florian_instance"
+  }
+
 }
--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "ubuntu_ami_name" {
-    description = "This is the ami name I use for my EC2 instance"
-    default = "ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"
+  description = "This is the ami name I use for my EC2 instance"
+  default     = "ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"
 
 
-  
+
 }
 
 variable "ubuntu_ami_owner" {
@@ -11,26 +11,25 @@ variable "ubuntu_ami_owner" {
 }
 
 variable "vpc_id" {
-    default = "vpc_0ebcdb39f7a526ef9"
-  
+  default = "vpc-0ebcdb39f7a526ef9"
+
 }
 
 
 variable "vpc_ip_address" {
-    default = "172.31.0.0/16"
-  
+  default = "172.31.0.0/16"
+
 }
 
 
 variable "aws_region" {
 
-    default = "eu-west-3"
-  
+  default = "eu-west-3"
+
+}
+
+variable "my_instance_type" {
+  default = "t2.micro"
 }
 
 
-
-variable "my_key" {
-    default = file("~/.ssh/id_ed25519.pub")
-  
-}
